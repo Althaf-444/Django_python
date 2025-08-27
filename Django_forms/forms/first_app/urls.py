2ded8f78133dee84cce4a0a2bb1c146fadf52770
Althaf-444 <altmd444@gmail.com>
2025-08-27 02:55:33 -0700

    add

--- Django_forms/forms/first_app/urls.py
+++ Django_forms/forms/first_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.contect,name='home'),
     path('contect/', views.contect_viwe , name= 'contect'),
     path('contect/succsess', views.contect_success , name = 'content-success'),
+    
 
 ]
