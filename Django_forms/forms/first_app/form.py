bad75f1849b67af98fd377d1f5a6e6d5bcd3090a
Althaf-444 <altmd444@gmail.com>
2025-08-27 03:31:02 -0700

    add

--- Django_forms/forms/first_app/form.py
+++ Django_forms/forms/first_app/form.py
@@ -1,9 +1,9 @@
 from django import forms
 
 class Contect_form(forms.Form):
-    name = forms.CharField(max_length=100)
-    Email = forms.EmailField()
-    message = forms.CharField(widget = forms.Textarea)
+    name = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class':"form-control"}))
+    Email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    message = forms.CharField(widget = forms.Textarea(attrs={'class': 'form-control'}))
     
     def send_email(self):
         print(f"Send email from {self.cleaned_data['Email']} with message : {self.cleaned_data['message']}" )
